07767c29e4b664331faa5c2fa78d8cb4ecaab319
Ant-sss <mimo.antownette.0429@gmail.com>
2020-10-18 21:16:06 +0900

    itemsコントローラー設置

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,9 @@
-class Category < ApplicationRecord
+class Category < ActiveHash::Base
+  self.data = [
+    { id: 1, name: "選択" }, { id: 2, name: "食事"},
+    { id: 3, name: "睡眠" }, { id: 4, name: "投資"},
+    { id: 5, name: "リーダーシップ" }, { id: 6, name: "ロジカルシンキング"} ,
+    { id: 7, name: "心理学" }, { id: 8, name: "脳科学" },
+    { id: 9, name: "ビジネス" }, { id: 10, name: "子育て" }
+  ]
 end
